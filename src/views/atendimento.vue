9115548e9a69c4ee2256c7908080f106ae7b6daa
lip2d <filipe258anacleto@hotmail.com>
2022-12-07 09:16:17 -0300

    tela suporte

--- src/views/atendimento.vue
+++ src/views/atendimento.vue
@@ -1,9 +1,31 @@
 <template>
-    <div></div>
+  <div class="suporte">
+    <div class="textcolorr">
+      <a href="https://criarmeulink.com.br/u/1670412429">
+        <b>Entre em contato conosco clicando aqui.</b></a
+      >
+    </div>
+    <div><img src="../assets/images/booksad.png" class="imgbooksad" /></div>
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
+.suporte {
+  margin-top: 250px;
+  position: absolute;
+  margin-left: 37%;
+  text-align: center;
+  color: blue;
+}
+
+.textcolorr {
+  color: blue;
+}
+
+.imgbooksad {
+  height: 500px;
+  margin-top: 100px;
+}
 </style>
